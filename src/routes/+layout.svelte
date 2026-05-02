9e796fd11eb389969aa2c27a9d0279dab3ba55d0
Amir Yasin <ayasin@datatensor.com>
2026-05-02 15:10:10 -0600

    fix fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import ConfirmationDialog from '$lib/components/ConfirmationDialog.svelte';
 	import { confirmationStore } from '$lib/stores/confirmationStore';
 	import { onMount } from 'svelte';
